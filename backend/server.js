1ec59bd38e078bb39f7066d4bbf93bc907f5ad6e
Arief Gusti <arief.gusti@gmail.com>
2021-02-24 17:09:54 +0700

    init db and api

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const app = require('./app');
+const connectDB = require('./config/database');
 const dotenv = require('dotenv');
 
 // config
 dotenv.config({ path: 'backend/config/config.env' })
+
+//connect db
+connectDB();
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
